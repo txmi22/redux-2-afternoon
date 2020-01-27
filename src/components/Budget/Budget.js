88a76c7a7d5ebc86a9383ee6c6cb45eeed51c8a3
Tommy Manyvanh <tommy_manyvanh@yahoo.com>
2020-01-27 15:46:19 -0700

    first commit

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import Background from './../shared/Background/Background'
 import Chart1 from './../shared/Chart1';
 import Chart2 from './../shared/Chart2';
@@ -7,24 +8,34 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import {requestUserData} from './../../ducks/userReducer';
+import {requestBudgetData, addPurchase, removePurchase} from './../../ducks/budgetReducer';
 
 
 class Budget extends Component {
+  componentDidMount() {
+    this.props.requestUserData()
+    this.props.requestBudgetData()
+  }
 
   render() {
+    const {loading, purchases, budgetLimit} = this.props.budget;
+    const {firstName, lastName} = this.props.user;
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={this.props.addPurchase}/>
+              <DisplayPurchases purchases={purchases} removePurchase={this.props.removePurchase} />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases}
+                budgetLimit={budgetLimit}
+              />
+              <Chart2 purchases={purchases}/>
             </div>
           </div>
         </div>
@@ -33,4 +44,11 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+function mapStateToProps(state) {
+  return {
+    budget: state.budget,
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, {requestUserData, requestBudgetData, addPurchase, removePurchase})(Budget);
